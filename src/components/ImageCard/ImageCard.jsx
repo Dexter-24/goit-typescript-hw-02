f79dbfb7b4b22ca8ee7c4dab4ff853685390c36d
Denis Chernyak <67460733+Dexter-24@users.noreply.github.com>
2024-10-08 18:04:31 +0300

    Initial commit

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,9 @@
+import css from './ImageCard.module.css'
+
+export default function ImageCard({ image, onClick }) {
+    return (
+        <div className={css.card} >
+            <img src={image.urls.small} alt={image.alt_description} className={css.img} onClick={() => onClick(image)}/>
+    </div>
+    )
+}
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,7 @@
+import css from './LoadMoreBtn.module.css'
+
+export default function LoadMoreBtn({ onLoadMore }) {
+    return (
+        <button className={css.button} onClick={onLoadMore}>Load more</button>
+    );
+}
